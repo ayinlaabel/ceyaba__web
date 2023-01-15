30079c05a7c84d14ca3e7686881ab606ac86b1fb
Agburum Victor <victoragburum@gmail.com>
2023-01-15 09:51:07 -0600

    finished hero section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,24 @@
+import React from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import "../components/HomeStyles.css"
+import play from "../assests/play.png";
+
+
 
 const Home = () => {
 
     return(
         <>
-        <Navbar />
-        <div className="hero">
-            <div className="hero__content">
-                <h1> <span id="hero">Welcome to </span>christ embassy yaba</h1>
+            <Navbar />
+            <div className="hero">
+                <div className="hero__content">
+                    <h1> <span id="hero">Welcome to </span>christ embassy <span id="hero1">yaba Church </span> </h1>
 
-                <p>watch live</p>
+                   <div className="play__btn"><p>watch live</p>  <img src={play} alt="play"/></div> 
+                </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </>
     )
 }
