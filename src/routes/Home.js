87078b9aae6a83109f11b1fc9e68a65b73d280b4
Agburum Victor <victoragburum@gmail.com>
2023-01-21 10:14:58 -0600

    hero section completed

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,26 +1,34 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import "../components/HomeStyles.css"
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "../components/HomeStyles.css";
 import play from "../assests/play.png";
-
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="hero">
+        <div className="hero__content">
+          <h1>
+            {" "}
+            <span id="hero">Welcome to </span>christ embassy{" "}
+            <span id="hero1">yaba Church </span>{" "}
+          </h1>
+          <Link to="/watchlive" style={{ textDecoration: "none" }}>
+            <button className="play__btn">
+              <span>watch live</span>
+              <div className="play">
+                <img src={play} alt="play" />
+              </div>
+            </button>
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-    return(
-        <>
-            <Navbar />
-            <div className="hero">
-                <div className="hero__content">
-                    <h1> <span id="hero">Welcome to </span>christ embassy <span id="hero1">yaba Church </span> </h1>
-
-                   <div className="play__btn"><p>watch live</p>  <img src={play} alt="play"/></div> 
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
-}
-
-export default Home
+export default Home;
